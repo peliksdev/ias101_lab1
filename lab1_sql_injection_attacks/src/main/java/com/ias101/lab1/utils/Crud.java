46f3d2c89f26f144ab62c9315990274b32421a2b
peliksdev <castillojohnfelix20@gmail.com>
2025-02-27 16:36:08 +0800

    Add files via upload

--- lab1_sql_injection_attacks/src/main/java/com/ias101/lab1/utils/Crud.java
+++ lab1_sql_injection_attacks/src/main/java/com/ias101/lab1/utils/Crud.java
@@ -1,11 +1,12 @@
-package com.ias101.lab1.utils;
+
+ package com.ias101.lab1.utils;
 
 import com.ias101.lab1.database.util.DBUtil;
 import com.ias101.lab1.model.User;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Utility class for performing CRUD operations on user data in the database.
@@ -15,89 +16,73 @@ public class Crud {
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
 
-    // SQL queries as constants
-    private static final String SELECT_ALL_USERS = "SELECT * FROM user_data";
+    private static final Pattern SAFE_INPUT_PATTERN = Pattern.compile("^[a-zA-Z0-9!@#$%^&*()_+=-{}\\[\\]:;\"'<>,.?/~`|\\\\]+$");
 
-    /**
-     * Retrieves <b>all users</b> from the database.
-     *
-     * @return List of all User objects in the database
-     * @throws RuntimeException if there is an error fetching users from database
-     */
     public static List<User> getAll() {
-        ResultSet rs = null;
         List<User> users = new ArrayList<>();
+        try (Connection connection = DBUtil.connect(DB_URL, DB_USER, DB_PASSWORD);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM user_data")) {
 
-        try (var connection = DBUtil.connect(DB_URL, DB_USER, DB_PASSWORD);
-             Statement stmt = connection.createStatement()) {
-
-            rs = stmt.executeQuery(SELECT_ALL_USERS);
-            while(rs.next()) {
+            while (rs.next()) {
                 users.add(extractUserFromResultSet(rs));
             }
-            rs.close();
-            return users;
         } catch (SQLException e) {
             throw new RuntimeException("Error fetching all users", e);
         }
+        return users;
     }
 
-
-    /**
-     * Searches for a user by their <b>username</b>.
-     *
-     * @param username The username to search for
-     * @return User object if found, null otherwise
-     * @throws RuntimeException if there is an error searching the database
-     */
     public static User searchByUsername(String username) {
-        ResultSet rs = null;
+        if (!isValidInput(username)) {
+            throw new IllegalArgumentException("Invalid input detected.");
+        }
+        username = sanitize(username);
         User user = null;
+        try (Connection connection = DBUtil.connect(DB_URL, DB_USER, DB_PASSWORD);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM user_data WHERE username = '" + username + "'")) {
 
-        try (var connection = DBUtil.connect(DB_URL, DB_USER, DB_PASSWORD);
-             Statement stmt = connection.createStatement()) {
-
-            // Deliberately vulnerable to SQL injection for demonstration
-            rs = stmt.executeQuery(String.format("SELECT * FROM user_data WHERE username = '%s'", username));
-            while(rs.next()) {
+            if (rs.next()) {
                 user = extractUserFromResultSet(rs);
             }
-            rs.close();
-            return user;
         } catch (SQLException e) {
             throw new RuntimeException("Error searching for user: " + username, e);
         }
+        return user;
     }
 
-    /**
-     * Deletes a user from the database by their username.
-     *
-     * @param username The username of the user to delete
-     * @throws RuntimeException if there is an error deleting the user
-     */
     public static void deleteUserByUsername(String username) {
-        try (var connection = DBUtil.connect(DB_URL, DB_USER, DB_PASSWORD);
+        if (!isValidInput(username)) {
+            throw new IllegalArgumentException("Invalid input detected.");
+        }
+        username = sanitize(username);
+
+        try (Connection connection = DBUtil.connect(DB_URL, DB_USER, DB_PASSWORD);
              Statement stmt = connection.createStatement()) {
 
-            // Deliberately vulnerable to SQL injection for demonstration
-            stmt.execute(String.format("DELETE FROM user_data WHERE username = '%s'", username));
+            String query = "DELETE FROM user_data WHERE username = '" + username + "'";
+            int affectedRows = stmt.executeUpdate(query);
 
+            if (affectedRows > 0) {
+                System.out.println("User deleted successfully.");
+            } else {
+                System.out.println("User not found.");
+            }
         } catch (SQLException e) {
             throw new RuntimeException("Error deleting user: " + username, e);
         }
     }
 
-    /**
-     * Extracts user data from a ResultSet and creates a User object.
-     *
-     * @param rs The ResultSet containing user data
-     * @return User object created from the ResultSet data
-     * @throws SQLException if there is an error reading from the ResultSet
-     */
+    private static boolean isValidInput(String input) {
+        return SAFE_INPUT_PATTERN.matcher(input).matches();
+    }
+
+    private static String sanitize(String input) {
+        return input.replace("'", "''"); // Escaping single quotes to prevent SQL injection
+    }
+
     private static User extractUserFromResultSet(ResultSet rs) throws SQLException {
-        return new User(
-                rs.getString("username"),
-                rs.getString("password")
-        );
+        return new User(rs.getString("username"), rs.getString("password"));
     }
 }
